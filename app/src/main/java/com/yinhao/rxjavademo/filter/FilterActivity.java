a709f54d37eb79621a6c7dfaf920874875da884e
yinhao <sdwfhjq123@qq.com>
2018-01-30 17:12:18 +0800

    finish

--- app/src/main/java/com/yinhao/rxjavademo/filter/FilterActivity.java
+++ app/src/main/java/com/yinhao/rxjavademo/filter/FilterActivity.java
@@ -30,13 +30,13 @@ public class FilterActivity extends AppCompatActivity {
         mTextView = (TextView) findViewById(R.id.text_view);
         mEditText = (EditText) findViewById(R.id.edit_text);
 
-        /*
+        /**
          * 说明
          * 1. 此处采用了RxBinding：RxTextView.textChanges(name) = 对对控件数据变更进行监听（功能类似TextWatcher），
          * 需要引入依赖：compile 'com.jakewharton.rxbinding2:rxbinding:2.0.0'
          * 2. 传入EditText控件，输入字符时都会发送数据事件（此处不会马上发送，因为使用了debounce（））
          * 3. 采用skip(1)原因：跳过 第1次请求 = 初始输入框的空字符状态
-         **/
+         */
         RxTextView.textChanges(mEditText)
                 .debounce(1, TimeUnit.SECONDS)//.skip(1)
                 .observeOn(AndroidSchedulers.mainThread())
